9eb7856550907b64b0a4ad9ddc741dfdf36c3c36
rusgyn <rusgynmagsanay@gmail.com>
2024-02-05 20:23:03 -0500

    Refactor. Exported the function to index.js using module.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,21 @@
 const request = require('request');
-const process = require('process');
 
-let args = process.argv;
-let breedName = args[2];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const makeRequest = function(url, responseHandler) {
-  //The underscore(_) written before error and response to represent that these parameters are not in use but needed as part of the `request` syntax.
   request(url, (error, _response, body) => {
     if (error) {
-      console.log(error); //Request failed, e.g. breaking the given URL.
+      callback(error, null);
     } else {
-      const data = JSON.parse(body);
-      responseHandler(data); // callback
+      let result = JSON.parse(body);
+      let output = "";
+      for (let x of result) {
+        output = x.description;
+      }
+      //let result = JSON.parse(body).length > 1 ? JSON.parse(body)[0].description : 'No results'
+      callback(null, output);
     }
   });
 };
 
-
-//The callback function, will do the process of showing the requested breed
-const responseHandler = function(response) {
-  if (response.length > 0) {
-    //access the data array and output the description for the user
-    for (let entry of response) {
-      console.log(entry.description);
-    }
-  } else {
-    //edge case: requested breed not found.
-    console.log(`Breed Not Found, breed name: ${breedName}.`);
-  }
-};
-
-makeRequest(url, responseHandler);
+module.exports = { fetchBreedDescription };
